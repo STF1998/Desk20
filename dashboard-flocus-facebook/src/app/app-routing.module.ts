eddfb2636cc09cb5716905f3755626ff76e8e201
sf17177 <sf17177@bristol.ac.uk>
2021-04-10 12:37:54 +0100

    add league and study components - the login is now routed to home

--- dashboard-flocus-facebook/src/app/app-routing.module.ts
+++ dashboard-flocus-facebook/src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoadingAnimComponent } from './loading-anim/loading-anim.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
