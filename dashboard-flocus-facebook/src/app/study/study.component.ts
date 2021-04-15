398ecdf4e34649268588af6f1f4d50dc673bd6e8
Gordon Tse <gordon@pc-74-92.customer.ask4.lan>
2021-04-15 16:04:38 +0100

    added query to db to get glasscnt

--- dashboard-flocus-facebook/src/app/study/study.component.ts
+++ dashboard-flocus-facebook/src/app/study/study.component.ts
@@ -1,3 +1,4 @@
+import { HttpParams } from '@angular/common/http';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription, timer } from 'rxjs';
 import { DataService } from '../data.service';
@@ -7,9 +8,14 @@ import { DataService } from '../data.service';
   templateUrl: './study.component.html',
   styleUrls: ['./study.component.css']
 })
+
 export class StudyComponent implements OnInit, OnDestroy {
 
+  public dayGlassCount: number; /*  = 8 */;
   public waterLevel: number;  // used for rendering on frontend (time passed / total study session time)
+  
+  private userid = "testglasscnt"; // tmp (will be retrieved from the login component)
+  
   private studyTime = 10;     // Amendable: the amount of time for a study session in seconds (aka 45 mins)
   private breakTime = 5;      // Amendable: the amount of time for a break session in seconds (aka 15 mins)
   private updatefreq = 2;     // Amendable: the frequency of updating the waterLevel variable for rendering (in seconds)
@@ -17,16 +23,18 @@ export class StudyComponent implements OnInit, OnDestroy {
   private time: Subscription = Subscription.EMPTY;
   private timePassed = 0;
   private isStudy = true;
+  private stats: any = [];
 
   constructor(private DataService: DataService) { }
 
   ngOnInit(): void {
+    this.retrieveGlassCount();
   }
 
   ngOnDestroy(): void {
     if(this.timePassed > 0){
-      console.log(JSON.stringify({ "uid": "sampeluid", "timestamp": new Date(), "timeSpent": this.timePassed }));  // posting to db
-      this.DataService.postRecord(JSON.stringify({ "uid": "sampeluid", "timestamp": new Date(), "timeSpent": this.timePassed }));
+      console.log(JSON.stringify({ "uid": this.userid, "timestamp": new Date(), "timeSpent": this.timePassed }));  // posting to db
+      this.DataService.postRecord(JSON.stringify({ "uid": this.userid, "timestamp": new Date(), "timeSpent": this.timePassed }));
       this.pauseTimer();
     }
   }
@@ -49,8 +57,8 @@ export class StudyComponent implements OnInit, OnDestroy {
           console.log(this.waterLevel);  // tmp
         }
         if (this.timePassed == this.studyTime) {
-          console.log(JSON.stringify({ "uid": "sampeluid", "timestamp": new Date(), "timeSpent": this.timePassed }));  // posting to db
-          this.DataService.postRecord(JSON.stringify({ "uid": "sampeluid", "timestamp": new Date(), "timeSpent": this.timePassed }));
+          console.log(JSON.stringify({ "uid": this.userid, "timestamp": new Date(), "timeSpent": this.timePassed }));  // posting to db
+          this.DataService.postRecord(JSON.stringify({ "uid": this.userid, "timestamp": new Date(), "timeSpent": this.timePassed }));
           this.timePassed = 0;
           this.isStudy = false;
         }
@@ -72,6 +80,30 @@ export class StudyComponent implements OnInit, OnDestroy {
     }
   }
 
-  dayGlassCount = 8;
+  private retrieveGlassCount(){
+
+    var date = new Date();
+    var dayStart = new Date(date.setHours(0,0,0,0));
+    var dayEnd = new Date(date.setHours(23,59,59,999));
+
+    var httpParams = new HttpParams()
+    .set("uid", this.userid)
+    .set("dayStart", JSON.parse(JSON.stringify(dayStart)))
+    .set("dayEnd", JSON.parse(JSON.stringify(dayEnd)))
+    .set("timeSpent", this.studyTime.toString());
+
+    this.DataService.getRecord(httpParams).subscribe(
+      data => {
+        this.stats = data;
+        this.dayGlassCount = this.stats.length;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+    
+    
+  }
+  
 
 }
