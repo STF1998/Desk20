9d6cdde6f86cf7cfc494ab50c9d42b9569e531dc
tomcockain <tomcockain@gmail.com>
2021-04-17 17:28:42 +0200

    added get specific user to api

--- dashboard-flocus-facebook/server/routes/user.js
+++ dashboard-flocus-facebook/server/routes/user.js
@@ -12,6 +12,13 @@ router.get('/', async(req, res) => {
         res.status(500).json({ message: err.message})
     }
 });
-
+router.get('/:id', async(req, res) => {
+    try{
+        const users = await User.findById(req.params.id)
+        res.json(users)
+    } catch (err) {
+        res.status(500).json({ message: err.message})
+    }
+});
 module.exports = router;
  
