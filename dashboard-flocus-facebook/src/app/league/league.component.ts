1c14fc2f26248ef2428b2dc008d378545d337e53
sf17177 <sf17177@bristol.ac.uk>
2021-04-19 10:53:40 +0100

    added lottie anim on league page and started personal stats

--- dashboard-flocus-facebook/src/app/league/league.component.ts
+++ dashboard-flocus-facebook/src/app/league/league.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GoogleChartsModule } from 'angular-google-charts';
 
 @Component({
   selector: 'app-league',
@@ -12,4 +13,26 @@ export class LeagueComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public drawChart() {
+    var data = google.visualization.arrayToDataTable([
+        ['Day', 'Glasses'],
+        ['Mo', 3],
+        ['Tu', 5],
+        ['We', 6],
+        ['Th', 0],
+        ['Fr', 0],
+        ['Sa', 1]
+    ]);
+    var options = {
+        title: 'Glasses poured per day',
+        isStacked: true
+    };
+    var id = document.getElementById('stats');
+    if(id == null){
+      alert("cannot find stats container");
+      return;
+    }
+    var chart = new google.visualization.BarChart(id);
+    chart.draw(data, options);
+  }
 }
