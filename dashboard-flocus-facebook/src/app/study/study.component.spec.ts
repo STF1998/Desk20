c3d939301bf3dc1e4359dc243be766ebe0a8198b
Gordon Tse <gordon@pc-74-92.customer.ask4.lan>
2021-05-05 00:28:41 +0100

    Some trivial testing has been done

--- dashboard-flocus-facebook/src/app/study/study.component.spec.ts
+++ dashboard-flocus-facebook/src/app/study/study.component.spec.ts
@@ -1,9 +1,7 @@
 
 import { HttpClientModule } from '@angular/common/http';
 import { DebugElement, inject } from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { data } from 'jquery';
-import { By } from 'protractor';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { Observable, of } from 'rxjs';
 import { DataService } from '../data.service';
 
@@ -20,19 +18,34 @@ describe('StudyComponent', () => {
   let component: StudyComponent;
   let fixture: ComponentFixture<StudyComponent>;
   let html: DebugElement;
-  let dataService: DataService;
+  // let dataService: DataService;
+  let getUidSpy: any;
+  let getRecordSpy: any;
 
   beforeEach(async () => {
+    const dataService = jasmine.createSpyObj('DataService', ['getUid', 'getRecord']);
+    getUidSpy = dataService.getUid.and.returnValue(of("test"));
+    const returnObj = JSON.parse(JSON.stringify([{
+      _id: "test",
+      session: 100,
+      timeSpent: 1000
+    }]));
+    console.log(returnObj);
+    getRecordSpy = dataService.getRecord.and.returnValue(of(returnObj));
+    console.log(getRecordSpy);
+
+
+
     await TestBed.configureTestingModule({
       imports: [HttpClientModule],
       declarations: [StudyComponent],
-      providers: [DataService/* , MockDataService */]
+      providers: [{ provide: DataService, useValue: dataService }]
     })
       .compileComponents().then(() => {
         fixture = TestBed.createComponent(StudyComponent);
         component = fixture.componentInstance;
         html = fixture.debugElement;
-        dataService = TestBed.inject(DataService);
+        // dataService = TestBed.inject(dataService);
       });
 
     fixture.detectChanges();
@@ -43,35 +56,39 @@ describe('StudyComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  // can't get it success...
-  it('should load user data', async () => {
-    spyOn(component, 'retrieveUidWithUserData').and.callFake(()=>{
-      const returnObj = JSON.stringify({
-        _id: null,
-        session: 100,
-        timeSpent: 1000
-      })
-      return of(returnObj);
-    });
-    expect(component.retrieveUidWithUserData).toHaveBeenCalled();
+
+  it('should getUid', async () => {
+    fixture.detectChanges();
+    expect(component.userid).toBe("test");
+    expect(getUidSpy.calls.any()).toBe(true, "getUid called");
   });
 
-  it('should start timer after the button is pressed', async () => {
-    const test = new StudyComponent(dataService);
-    expect(component.pressed).toBe(false);
-    
-    component.press();
-    spyOn(component, 'startTimer');
-    expect(component.pressed).toBe(true);
-    expect(component.startTimer).toHaveBeenCalled();
-    component.press();
-    expect(component.pressed).toBe(false);
-    // expect(component.press).toHaveBeenCalled();
-    // expect(component.startTimer).toHaveBeenCalled();
-    // expect(component.dripDrop).toHaveBeenCalledOnceWith("start");
-    // expect(component.fillUp).toHaveBeenCalled();
+  // can't get it success...
+  it('should load user data', async () => {
+    fixture.detectChanges();
+    expect(component.dayGlassCount).toEqual(100);
+    expect(getRecordSpy.calls.any()).toBe(true, "getRecord called");
+  });
 
+  it('should start timer after the button is pressed', () => {
+    expect(component.pressed).toBeFalsy();
+    let button = fixture.nativeElement.querySelector('button');
+    button.click();
+    expect(component.pressed).toBeTruthy();
+    button.click();
+    expect(component.pressed).toBeFalsy();
   })
+
+  it('timer should start counting when called', fakeAsync(() => {
+    let button = fixture.nativeElement.querySelector('button');
+    button.click();
+    setTimeout(() => {
+      button.click();
+    }, 4000);
+    tick(4000);
+    expect(component.pressed).toBeFalsy;
+    expect(component.yPos).toBeLessThan((-625 * 2 / 1500));
+  }))
 
 });
 
